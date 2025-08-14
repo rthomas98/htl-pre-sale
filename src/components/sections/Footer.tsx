9a9681ca098e63a191e2cde5673cee7b7426621e
Rob Thomas <robt84@gmail.com>
2025-08-14 13:09:18 -0500

    fixed footer copyright text

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 
 export function Footer() {
+  const currentYear = new Date().getFullYear();
+  
   return (
     <footer className="px-[5%] py-12 md:py-18 lg:py-20 bg-brand-white">
       <div className="container">
@@ -17,7 +19,7 @@ export function Footer() {
         </div>
         <div className="h-px w-full bg-brand-gray opacity-20" />
         <div className="font-body text-sm flex flex-col-reverse items-center justify-between pt-6 pb-4 text-center text-brand-black md:flex-row md:pt-8 md:pb-0">
-          <p className="mt-8 md:mt-0">© 2024 HTL Re-Sale. All rights reserved.</p>
+          <p className="mt-8 md:mt-0">© {currentYear} Hebert-Thomas Law, PLLC. All rights reserved.</p>
           <ul className="font-body text-sm grid grid-flow-row grid-cols-[max-content] justify-center gap-y-4 text-brand-black md:grid-flow-col md:gap-x-6 md:gap-y-0">
             <li className="underline hover:text-brand-red transition-colors">
               <a href="https://hebertthomaslaw.com/privacy-policy/" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
